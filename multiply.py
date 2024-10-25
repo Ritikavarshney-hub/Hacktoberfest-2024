0c39c3e73d420e86cd0f5cf79ab41b0f3174a731
Ritikavarshney-hub <varshneyritika15@gmail.com>
2024-10-26 01:12:41 +0530

    Create multiply.py

---
+++ multiply.py
@@ -0,0 +1,15 @@
+# function defined
+def multiply_num(a):
+    b = 40
+    r = a*b
+    return r
+
+
+# drivercode
+# assigning function
+z = multiply_num
+
+# invoke function
+print(z(6))
+print(z(10))
+print(z(100))
